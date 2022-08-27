787d64617a3fc755e5532663105cc536d54e5d4f
Wajih Rizvi <swh2rizv@edu.uwaterloo.ca>
2022-08-27 14:22:29 -0400

    Initial Commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Wajih Rizvi on 2022-08-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Wajih Rizvi on 2022-08-27.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
